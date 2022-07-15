5a157445e88e462735e753782d7579eba4e3e250
yueliu <yueliu@longtugame.com>
2022-07-15 18:24:13 +0800

    添加首页

--- routes/router.go
+++ routes/router.go
@@ -4,12 +4,26 @@ import (
 	"gintest/utils"
 	"net/http"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
+
+func createMyRender() multitemplate.Renderer {
+	p := multitemplate.NewRenderer()
+	p.AddFromFiles("front", "web/index.html")
+	return p
+}
 
 func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
+	r.HTMLRender = createMyRender()
+	r.Static("/static", "./web")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "front", nil)
+	})
+
 	router := r.Group("api/v2")
 	{
 		router.GET("hello", func(c *gin.Context) {
