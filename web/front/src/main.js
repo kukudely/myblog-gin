0b0944de061ef5f7236a240ad483b3d099a20a1f
liuyue <344776209@qq.com>
2022-07-22 14:22:50 +0800

    删除前端代码

--- web/front/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-createApp(App).use(ElementPlus).mount('#app')
--- web/front/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    devServer: {
-        // true 则热更新，false 则手动刷新，默认值为 true
-        hot:false,
-        inline: false,
-    }
-}
