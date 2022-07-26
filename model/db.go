2529fbf631edcf66a8d4cae50910d50af45d55eb
liuyue <344776209@qq.com>
2022-07-26 18:19:42 +0800

    获取个人信息

--- model/db.go
+++ model/db.go
@@ -21,7 +21,7 @@ func InitDb() {
 		fmt.Print("连接数据库失败，请检查参数", err)
 	}
 
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Category{}, &Article{}, &Profile{})
 
 	// 获取通用数据库对象 sql.DB ，然后使用其提供的功能
 	sqlDB, errDb := db.DB()
--- routes/router.go
+++ routes/router.go
@@ -19,9 +19,43 @@ func InitRouter() {
 	r := gin.Default()
 	r.HTMLRender = createMyRender()
 	r.Static("/assets", "./web/dist/assets")
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "front", nil)
 	})
-	r.POST("/login_ajax_check", v1.LoginFront)
+
+	/*
+		前端展示页面接口
+	*/
+	router := r.Group("api/v1")
+	{
+		// 用户信息模块
+		// router.POST("user/add", v1.AddUser)
+		// router.GET("user/:id", v1.GetUserInfo)
+		// router.GET("users", v1.GetUsers)
+
+		// 文章分类信息模块
+		// router.GET("category", v1.GetCate)
+		// router.GET("category/:id", v1.GetCateInfo)
+
+		// 文章模块
+		// router.GET("article", v1.GetArt)
+		// router.GET("article/list/:id", v1.GetCateArt)
+		// router.GET("article/info/:id", v1.GetArtInfo)
+
+		// 登录控制模块
+		// router.POST("login", v1.Login)
+		router.POST("loginfront", v1.LoginFront)
+
+		// 获取个人设置信息
+		router.GET("profile/:id", v1.GetProfile)
+
+		// 评论模块
+		// router.POST("addcomment", v1.AddComment)
+		// router.GET("comment/info/:id", v1.GetComment)
+		// router.GET("commentfront/:id", v1.GetCommentListFront)
+		// router.GET("commentcount/:id", v1.GetCommentCount)
+	}
+
 	r.Run(utils.HttpPort)
 }
